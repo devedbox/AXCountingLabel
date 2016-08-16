4bd9d433136b5c31b716b291a70b8e1571d96a1f
aiXing <862099730@qq.com>
2016-08-16 11:10:39 +0800

    Initial Commit

---
+++ AXCountingLabel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AXCountingLabel
+//
+//  Created by devedbox on 16/8/16.
+//  Copyright © 2016年 devedbox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AXCountingLabel/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  AXCountingLabel
+//
+//  Created by devedbox on 16/8/16.
+//  Copyright © 2016年 devedbox. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
